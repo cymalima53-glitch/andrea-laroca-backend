8f6637fd4cda316a46c5c4e2059b62d1cafc908e
kamalsolimanahmed-ui <kamalsolimanahmed@gmail.com>
2026-04-02 21:49:09 -0400

    update backend

--- src/components/layout/Header.tsx
+++
@@ -1,8 +0,0 @@
-import type { Locale } from '../../i18n-config';
-import { getDictionary } from '@/lib/get-dictionary';
-import HeaderClient from './HeaderClient';
-
-export default async function Header({ lang }: { lang: Locale }) {
-    const dict = await getDictionary(lang);
-    return <HeaderClient lang={lang} dict={dict} />;
-}
